d4ce1b27d82189ef16dbb3f23abaefa8aa2a618d
Manav Rathi <manav@mnvr.in>
2025-12-19 20:13:11 +0530

    xl

--- c2.py
+++ c2.py
@@ -1,14 +1,22 @@
 from transformers import pipeline
 
 gpt2 = pipeline('text-generation', model='gpt2')
+gpt2xl = pipeline('text-generation', model='gpt2-xl')
 smol = pipeline('text-generation', model='HuggingFaceTB/SmolLM2-135M')
 
 def show(seqs):
     print('\n•\n'.join(s['generated_text'] for s in seqs))
 
+def make(label, f):
+    def g(prompt):
+        print(f"\n{label} ==\n")
+        return show(f(prompt, max_new_tokens=150, num_return_sequences=3))
+    return g
+
 while True:
     prompt = input(">>> ")
-    print("gpt2:")
-    show(smol(prompt, max_new_tokens=150, num_return_sequences=3))
-    print("smol:")
-    show(gpt2(prompt, max_new_tokens=150, num_return_sequences=3))
+    gen = [make("gpt2", gpt2),
+           make("gpt2-xl", gpt2xl),
+           make("smol", smol)]
+    for g in gen:
+        g(prompt)
